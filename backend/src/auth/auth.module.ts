f258cad42fc95ec9bc0ff1d2f5ca7f6f8d6c8969
Cavallin021 <joaovictorcavallin07@gmail.com>
2026-06-03 15:12:05 -0300

    fix: userModule

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UsersService } from '../userstestes/users.service';
+import { UsersService } from '../users/users.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { env } from 'node:process';
 
